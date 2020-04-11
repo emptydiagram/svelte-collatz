c8cb2ea962e14492f73bb0d7c5065a9c25620691
Nick Hamann <nah0024@auburn.edu>
2020-04-10 23:26:17 -0400

    Implement counter app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,16 @@
 <script>
 	export let name;
+
+  let counter = 0;
+
+  function incCounter() {
+    counter += 1;
+  }
+
+  function decCounter() {
+    counter -= 1;
+  }
+
 </script>
 
 <style>
@@ -9,3 +20,10 @@
 </style>
 
 <h1>Hello {name}!</h1>
+
+<h2>{ counter }</h2>
+
+<div>
+  <button on:click={incCounter}>inc</button>
+  <button on:click={decCounter}>dec</button>
+</div>
