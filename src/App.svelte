e098d6fe8325d92e6b028ab39c46872e9972b967
Nick Hamann <nah0024@auburn.edu>
2020-04-11 00:08:39 -0400

    WIP: Add simple xstate machine.
    Not hooked up to UI.

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,56 @@
 <script>
+  import { Machine, interpret, assign } from 'xstate';
 	export let name;
 
   let counter = 0;
-
   function incCounter() {
     counter += 1;
   }
-
   function decCounter() {
     counter -= 1;
   }
 
+
+  const counterMachine = Machine({
+    id: 'counter',
+    initial: 'counting',
+    context: {
+      count: 7
+    },
+    states: {
+      counting: {
+        on: {
+          INC: {
+            actions: assign({
+              count: ctxt => ctxt.count + 1,
+            })
+          },
+          DEC: {
+            actions: assign({
+              count: ctxt => ctxt.count - 1,
+            })
+          },
+        }
+      }
+    },
+  });
+
+  const counterService = interpret(counterMachine);
+
+  let machineState = null;
+  counterService.onTransition(state => {
+    console.log("transitioning to context = ", state.context);
+    machineState = state;
+  });
+
+  counterService.start();
+  counterService.send('INC');
+  counterService.send('INC');
+  counterService.send('INC');
+  counterService.send('DEC');
+  counterService.send('INC');
+  counterService.stop();
+  console.log(machineState);
 </script>
 
 <style>
